e610cbf81f135633798eb996b6b1078261b776af
Abin Simon <abinsimon10@gmail.com>
2019-04-20 17:20:48 +0530

    fix some bugs

--- content.js
+++ content.js
@@ -1,7 +1,4 @@
-console.log('Sleed loaded. Happy faster playbackRate')
-
 function speedupVideos(speed) {
-  console.log('speed:', speed)
   // we don't know which one. So, why not all?
   const videos = Array.from(document.getElementsByTagName("video"));
   videos.forEach(v => {
@@ -10,6 +7,5 @@ function speedupVideos(speed) {
 }
 
 chrome.runtime.onMessage.addListener(function(request) {
-  console.log("request", request)
   speedupVideos(request);
 });
--- popup.js
+++ popup.js
@@ -1,5 +1,3 @@
-console.log('OK sleeding now.')
-
 function sendNotification(speed) {
   chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, speed);
@@ -9,14 +7,11 @@ function sendNotification(speed) {
 document.addEventListener(
   "DOMContentLoaded",
   function() {
-    // sendNotification(5)
     const slider = document.getElementById("slider");
     const valueDisplay = document.getElementById("display");
-    console.log(slider)
     slider.onchange = () => {
-      console.log(slider.value)
       sendNotification(slider.value);
-      valueDisplay.innerText = slider.value;
+      valueDisplay.innerText =  'Speed: ' + slider.value;
     };
   },
   false
