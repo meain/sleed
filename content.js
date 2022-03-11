92a2ff494760ed8fe8ad817a970386fb49829036
Abin Simon <mail@meain.io>
2022-03-11 11:35:30 +0530

    Add button and simplify styling

--- content.js
+++ content.js
@@ -1,11 +1,10 @@
 function speedupVideos(speed) {
-  // we don't know which one. So, why not all?
-  const videos = Array.from(document.getElementsByTagName("video"));
-  videos.forEach(v => {
-    v.playbackRate = speed;
-  });
+  // Let's just speed up all the videos in the page, what could go wrong?
+  Array.from(document.getElementsByTagName('video')).forEach((v) => {
+    v.playbackRate = speed
+  })
 }
 
-chrome.runtime.onMessage.addListener(function(request) {
-  speedupVideos(request);
-});
+chrome.runtime.onMessage.addListener(function (request) {
+  speedupVideos(request)
+})
--- popup.js
+++ popup.js
@@ -1,18 +1,30 @@
 function sendNotification(speed) {
-  chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, speed);
-  });
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, speed)
+  })
+}
+
+function updateSpeed(speed) {
+  sendNotification(speed)
+  document.getElementById('display').innerText = 'Speed: ' + speed
+  document.getElementById('slider').value = speed // when we update using buttons
 }
 
 document.addEventListener(
-  "DOMContentLoaded",
-  function() {
-    const slider = document.getElementById("slider");
-    const valueDisplay = document.getElementById("display");
-    slider.onchange = () => {
-      sendNotification(slider.value);
-      valueDisplay.innerText =  'Speed: ' + slider.value;
-    };
+  'DOMContentLoaded',
+  function () {
+    document.getElementById('slider').oninput = () => updateSpeed(slider.value)
+    document.getElementById('1').onclick = () => updateSpeed(1)
+    document.getElementById('1.5').onclick = () => updateSpeed(1.5)
+    document.getElementById('2').onclick = () => updateSpeed(2)
+    document.getElementById('2.25').onclick = () => updateSpeed(2.25)
+    document.getElementById('2.5').onclick = () => updateSpeed(2.5)
+    document.getElementById('2.75').onclick = () => updateSpeed(2.75)
+    document.getElementById('3').onclick = () => updateSpeed(3)
+    document.getElementById('3.25').onclick = () => updateSpeed(3.25)
+    document.getElementById('3.5').onclick = () => updateSpeed(3.5)
+    document.getElementById('3.75').onclick = () => updateSpeed(3.75)
+    document.getElementById('4').onclick = () => updateSpeed(4)
   },
   false
-);
+)
